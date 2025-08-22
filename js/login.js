ecb54a372653933f98c37d7398429cfcd00bbb8f
Agustin <AgustinGallotti23@gmail.com>
2025-08-22 20:37:51 -0300

    pulido de lógica en login

--- js/login.js
+++ js/login.js
@@ -1,22 +1,19 @@
 /* Modified by PowerChord25, todos los asientos reservados */
+document.addEventListener("DOMContentLoaded", function() {
+  const loginForm = document.getElementById("section-login");
 
-const loginForm = document.getElementById("section-login");     // Creo una constante para manejar la info del div del login (para el click del botón)
+  loginForm.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-const emailInput = document.getElementById("email");            // Convierto el correo en un objeto
-const passwordInput = document.getElementById("password");      // Convierto la contraseña en un objeto
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
+    if (email && password) {
+      localStorage.setItem("loggedIn", "true");
+      window.location.href = "index.html";
 
-loginForm.addEventListener("submit", function(event) {          // Le digo al DOM que esté a la escucha de que el botón envíe el evento "submit" (está en el login.html)
-    event.preventDefault();                                     // Según leí, esto previene que se recargue la página cuando se envíe el formulario
-
-    const email = emailInput.value;                             // Del objeto emailImput, saco el valor
-    const password = passwordInput.value;                       // Lo mismo para la contraseña
-
-    if (email === '' || password === '') {                      // Esto revisa si el email o la contraseña están vacíos
-        alert("Debes poner tu correo y contraseña");            // Y le alerta al usuario que tiene que completar
     } else {
-
-        localStorage.setItem("inicioSesion", "true");           // En caso que los campos no estén vacíos, guardo en localStorage la info de inicio de sesión, para que así no se borre si cierran la pestaña
-        window.location.href = "index.html";                    // Y ahora si movemos al usuario de vuelta al index.html
+      alert("Por favor, ingresa email y contraseña");
     }
+  });
 });
